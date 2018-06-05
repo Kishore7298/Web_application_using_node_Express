572573659e7a66af1b09c1c23faf1388986b30ed
Kishore7298 <4nm15is025@nmamit.in>
2018-06-05 08:33:32 +0530

    debug corrected

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var chalk = require('chalk')
-var debug = require('debug')
+var debug = require('debug')('app');
 
 var app = express();
 
@@ -9,5 +9,5 @@ app.get('/',function(req,res){
 });
 
 app.listen('3000',()=>{
-    debug(`listening from port ${chalk.green('3000')}`)
+    debug(`listening from port ${chalk.green('3000')}`);
 });
