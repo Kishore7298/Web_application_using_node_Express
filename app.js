f39689a4a23ddb5f8f398dd7941d604458e5c1d6
Kishore7298 <4nm15is025@nmamit.in>
2018-06-05 09:30:30 +0530

    Added morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var chalk = require('chalk')
 var debug = require('debug')('app');
+var morgan = require('morgan')
 
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/',function(req,res){
     res.send("Hello!");
