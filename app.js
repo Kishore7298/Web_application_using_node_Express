d0162218b303502523e500c056176fa9925685b9
Kishore7298 <4nm15is025@nmamit.in>
2018-06-05 11:09:05 +0530

    added Index.html  and used path package to host the static page

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express')
 var chalk = require('chalk')
 var debug = require('debug')('app');
 var morgan = require('morgan')
+var path = require('path')
 
 var app = express();
 
 app.use(morgan('tiny'));
 
 app.get('/',function(req,res){
-    res.send("Hello!");
+    res.sendFile(path.join(__dirname,'view','index.html'));
 });
 
 app.listen('3000',()=>{
